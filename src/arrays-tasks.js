ecb843c94bced7932f8cfc4980e11b7b96dda097
maksim <maksim20051708@gmail.com>
2024-01-14 00:26:37 +0300

    add: solved isSameLength

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -147,8 +147,9 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const firstWordLen = arr[0].length;
+  return arr.every((word) => word.length === firstWordLen);
 }
 
 /**

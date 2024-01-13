4e20c3b46a020b0284903a364a1c5286ca2923af
maksim <maksim20051708@gmail.com>
2024-01-14 01:44:54 +0300

    add: solved getInentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -414,8 +414,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (outerValue, index) =>
+    Array.from({ length: n }, (innerValue, innerIndex) =>
+      index === innerIndex ? 1 : 0
+    )
+  );
 }
 
 /**
